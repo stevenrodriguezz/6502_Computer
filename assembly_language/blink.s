f841af633c2badb895e55b1dd98b74bc65af8727
stevenrodriguezzz <stevenar135@gmail.com>
2024-10-13 22:07:24 -0400

    added instructions for printing H

--- assembly_language/blink.s
+++ assembly_language/blink.s
@@ -1,16 +1,70 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003 
+
+E = %10000000
+RW = %01000000
+RS = %00100000
+
  .org $8000
 
 reset: 
- lda #$ff
- sta $6002
+ lda #%11111111 ; Set all pins on port B to output
+ sta DDRB
 
- lda #$50
- sta $6000
+ lda #%11100000 ; Set top 3 pins on port A to output
+ sta DDRA
+
+ lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font 
+ sta PORTB
+
+ lda #0         ; Clear RS/RW/E bits 
+ sta PORTA
+
+ lda #E         ; Set E bit to send instructons
+ sta PORTA
+
+ lda #0         ; Clear RS/RW/E bits
+ sta PORTA
+
+ lda #%00001110 ; Display on; cursor on; blink off 
+ sta PORTB
+
+ lda #0         ; Clear RS/RW/E bits 
+ sta PORTA
+
+ lda #E         ; Set E bit to send instructons
+ sta PORTA
+
+ lda #0         ; Clear RS/RW/E bits
+ sta PORTA
+
+ lda #%00000110 ; Display on; cursor on; blink off 
+ sta PORTB
+
+ lda #0         ; Clear RS/RW/E bits 
+ sta PORTA
+
+ lda #E         ; Set E bit to send instructons
+ sta PORTA
+
+ lda #0         ; Clear RS/RW/E bits
+ sta PORTA
+
+ lda #"H"
+ sta PORTB
+
+ lda #RS        ; Clear RS/ \RW/E bits
+ sta PORTA 
+
+ lda #(RS | E)  ; Set E bit to send instructions 
+ sta PORTA
+
+ lda #RS        ; Clear RS/RW/E bits 
+ sta PORTA
 
 loop: 
- ror
- sta $6000
-
  jmp loop
 
  .org $fffc 
